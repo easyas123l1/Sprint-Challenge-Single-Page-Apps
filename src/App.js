1866544d45594a6670b51ef33e4dcd5b0a6222f2
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-10-18 10:40:08 -0500

    add WelcomePage to the home page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route } from 'react-router-dom';
 import Header from "./components/Header.js";
 import CharacterList from './components/CharacterList';
+import WelcomePage from './components/WelcomePage';
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <WelcomePage />
       <Route exact path='/' component={CharacterList} />
     </main>
   );
